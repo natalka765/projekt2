f97c6d50ba6994bd5c13769e0c0f7196aa3c5f92
natalka <natalka.kopiasz@o2.pl>
2024-04-30 12:57:22 +0200

    zmiany

--- script.js
+++ script.js
@@ -36,3 +36,52 @@ searchInput.addEventListener('input', function(){
         }
     });
 });
+
+let currentPage = 1;
+let recordsOnPage = 5; 
+const table = document.getElementById('server-table');
+
+function displayData() {
+    const startIndex = (currentPage - 1) * recordsOnPage;
+    const endIndex = startIndex + recordsOnPage;
+
+    for (let i = 1; i < rows.length; i++) {
+        rows[i].style.display = 'none';
+    }
+
+    for (let i = startIndex + 1; i < Math.min(endIndex + 1, rows.length); i++) {
+        rows[i].style.display = '';
+    }
+
+    document.getElementById('page-info').textContent = `strona ${currentPage}`;
+}
+
+function nextPage() {
+    if ((currentPage * recordsOnPage) < rows.length) {
+        currentPage++;
+        displayData();
+    }
+}
+
+function previousPage() {
+    if (currentPage > 1) {
+        currentPage--;
+        displayData();
+    }
+}
+
+function RecordsOnPage() {
+    recordsOnPage = parseInt(document.getElementById('RecordsOnPage').value);
+    currentPage = 1;
+    displayData();
+}
+window.onload = function() {
+    displayData();
+};
+var statusCells = document.querySelectorAll('#server-table td:last-child');
+
+statusCells.forEach(function(cell) {
+    cell.addEventListener('click', function() {
+        document.getElementById('bottom').scrollIntoView({ behavior: 'smooth' });
+    });
+});
